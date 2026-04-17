e90fd71d0f97099058db47d8bc59b7fb6cafed27
Himanshu <hmcreationhimanshu012@gmail.com>
2026-04-17 19:02:49 +0530

    added client and corrected it

--- budget_resource_service/src/main/java/com/finance/client/FinancialProgramClient.java
+++ budget_resource_service/src/main/java/com/finance/client/FinancialProgramClient.java
@@ -3,13 +3,14 @@ package com.finance.client;
 import java.math.BigDecimal;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.finance.dto.FinancialProgramResponseDTO;
 
 @FeignClient(name = "financial-program-subsidy-service")
-
+@Profile("dev")
 public interface FinancialProgramClient {
 
 	@GetMapping("/programs/fetch/{programId}")
@@ -17,5 +18,5 @@ public interface FinancialProgramClient {
 
 	@GetMapping("/subsidies/approved_sum/{programId}")
 	BigDecimal getApprovedAmount(@PathVariable Long programId);
-	
+
 }

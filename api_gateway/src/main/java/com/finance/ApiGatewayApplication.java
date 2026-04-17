82268113608e66f46cd3bf6db4751afb2ffa2195
Himanshu <hmcreationhimanshu012@gmail.com>
2026-04-17 16:35:25 +0530

    added version

--- api_gateway/src/main/java/com/finance/ApiGatewayApplication.java
+++ api_gateway/src/main/java/com/finance/ApiGatewayApplication.java
@@ -2,6 +2,9 @@ package com.finance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -9,5 +12,6 @@ public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
+	
 
 }

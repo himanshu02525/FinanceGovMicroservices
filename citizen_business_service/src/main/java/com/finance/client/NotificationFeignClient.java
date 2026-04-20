d241486157eecdca4e53fbdb357efe22306bf8f6
Areef <mohammadareef3326@gmail.com>
2026-04-20 10:48:28 +0530

    commmit

--- citizen_business_service/src/main/java/com/finance/client/NotificationFeignClient.java
+++ citizen_business_service/src/main/java/com/finance/client/NotificationFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
- 
+ //
 @FeignClient(
     name = "notification-service",
     url = "${notification.service.url}"

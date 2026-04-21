41e134b92ad29ef90667359dc13bee8a35502e32
Himanshu <hmcreationhimanshu012@gmail.com>
2026-04-21 14:57:44 +0530

    Corrected the feign client , removed Profile

--- budget_resource_service/src/main/java/com/finance/client/FinancialProgramClient.java
+++ budget_resource_service/src/main/java/com/finance/client/FinancialProgramClient.java
@@ -3,20 +3,18 @@ package com.finance.client;
 import java.math.BigDecimal;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.finance.dto.FinancialProgramResponseDTO;
 
 @FeignClient(name = "financial-program-subsidy-service")
-@Profile("dev")
 public interface FinancialProgramClient {
 
 	@GetMapping("/programs/fetch/{programId}")
-	FinancialProgramResponseDTO getProgramById(@PathVariable Long programId);
+	FinancialProgramResponseDTO getProgramById(@PathVariable("programId") Long programId);
 
 	@GetMapping("/subsidies/approved_sum/{programId}")
-	BigDecimal getApprovedAmount(@PathVariable Long programId);
+	BigDecimal getApprovedAmount(@PathVariable("programId") Long programId);
 
 }

03200dc3bc60c20b2d65d00207e1354c2e9cc746
Areef <mohammadareef3326@gmail.com>
2026-04-24 17:28:40 +0530

    commit

--- report_generation_service/src/main/java/com/finance/dto/ReportResponseDTO.java
+++ report_generation_service/src/main/java/com/finance/dto/ReportResponseDTO.java
@@ -3,12 +3,15 @@ package com.finance.dto;
 import java.time.LocalDateTime;
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.finance.enums.ReportScope;
 import lombok.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(value = Include.NON_NULL)
 public class ReportResponseDTO {
 
     private Long reportId;

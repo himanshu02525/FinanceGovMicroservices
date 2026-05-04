170ad324b87980f141a44646036ae3d1a5db098f
Aravind <gaddamaravind540@gmail.com>
2026-05-04 15:59:34 +0530

    feign

--- compliance_audit_service/src/main/java/com/finance/client/TaxFeignClient.java
+++ compliance_audit_service/src/main/java/com/finance/client/TaxFeignClient.java
@@ -3,12 +3,13 @@ package com.finance.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.finance.dto.TaxResponseDTO;
 
 @FeignClient(name = "taxation-disclosure-service")
 public interface TaxFeignClient {
 
-	@GetMapping("taxation/taxerecords/{taxID}")
-	ResponseEntity<TaxResponseDTO> getTaxById(Long ref);
+	@GetMapping("/api/taxation/taxrecords/{id}")
+	ResponseEntity<TaxResponseDTO> getTaxById(@PathVariable Long id);
 }
